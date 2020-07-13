6af1ba39aac3807351de556c5ea56b912e4971ea
PosaLusa24 <posalusa24@gmail.com>
2020-07-13 22:43:27 +0630

    Use new function name

--- tests/cpu.test.cpp
+++ tests/cpu.test.cpp
@@ -6,15 +6,15 @@ int16_t reg_ram;
 
 TEST_CASE("Test A-instruction and pc", "[1]") {
     cpu.reset();
-    cpu.process("@69", reg_ram);
+    cpu.execute("@69", reg_ram);
     REQUIRE(cpu.get_reg_a() == 69);
     REQUIRE(cpu.get_pc() == 1);
 }
 
 TEST_CASE("Test dest (AMD) and pc", "[2]") {
     cpu.reset();
-    cpu.process("@69", reg_ram);
-    cpu.process("AMD=A", reg_ram);
+    cpu.execute("@69", reg_ram);
+    cpu.execute("AMD=A", reg_ram);
     REQUIRE(cpu.get_reg_a() == 69);
     REQUIRE(cpu.get_reg_d() == 69);
     REQUIRE(reg_ram == 69);
@@ -23,9 +23,9 @@ TEST_CASE("Test dest (AMD) and pc", "[2]") {
 
 TEST_CASE("Test comp (A+M)", "[3]") {
     cpu.reset();
-    cpu.process("@60", reg_ram);
-    cpu.process("M=A", reg_ram);
-    cpu.process("D=A+M", reg_ram);
+    cpu.execute("@60", reg_ram);
+    cpu.execute("M=A", reg_ram);
+    cpu.execute("D=A+M", reg_ram);
     REQUIRE(cpu.get_reg_a() == 60);
     REQUIRE(cpu.get_reg_d() == 120);
     REQUIRE(reg_ram == 60);
@@ -33,26 +33,26 @@ TEST_CASE("Test comp (A+M)", "[3]") {
 
 TEST_CASE("Test jump (JMP)", "[4]") {
     cpu.reset();
-    cpu.process("@60", reg_ram);
-    cpu.process("0;JMP", reg_ram);
+    cpu.execute("@60", reg_ram);
+    cpu.execute("0;JMP", reg_ram);
     REQUIRE(cpu.get_pc() == 60);
 }
 
 TEST_CASE("Test jump (JGT)", "[5]") {
     cpu.reset();
-    cpu.process("@12", reg_ram);
-    cpu.process("D=A", reg_ram);
-    cpu.process("@60", reg_ram);
-    cpu.process("D;JGT", reg_ram);
+    cpu.execute("@12", reg_ram);
+    cpu.execute("D=A", reg_ram);
+    cpu.execute("@60", reg_ram);
+    cpu.execute("D;JGT", reg_ram);
     REQUIRE(cpu.get_pc() == 60);
 }
 
 TEST_CASE("Test multicommand", "[6]") {
     cpu.reset();
-    cpu.process("@0", reg_ram);
-    cpu.process("D=A", reg_ram);
-    cpu.process("@60", reg_ram);
-    cpu.process("MD=D-1;JLT", reg_ram);
+    cpu.execute("@0", reg_ram);
+    cpu.execute("D=A", reg_ram);
+    cpu.execute("@60", reg_ram);
+    cpu.execute("MD=D-1;JLT", reg_ram);
     REQUIRE(cpu.get_pc() == 60);
     REQUIRE(cpu.get_reg_d() == -1);
     REQUIRE(reg_ram == -1);
